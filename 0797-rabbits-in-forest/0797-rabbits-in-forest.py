6dfa0861fb3143409a64dfb515c099a58d202b84
ABHILASHVM <abhilashvm44@gmail.com>
2025-04-20 20:39:02 +0530

    Time: 3 ms (27.92%), Space: 17.9 MB (76.07%) - LeetHub

---
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        # a=list(set(answers))
+        # return sum(a)+len(a)
+        mpp = Counter(answers)
+        total = 0
+        for x in mpp:
+            total += ceil(float(mpp[x]) / (x + 1)) * (x + 1)
+        return int(total)
+        
